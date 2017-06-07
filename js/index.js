b9a1eb0ded4fc4e1dfac8babf72fe695f69d5968
Mayerson <mayerson18@gmail.com>
2017-06-06 21:56:00 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,139 @@
+var data = [
+  {
+   title: 'FRONT DESK',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'DECISIONES SUPPORT SYSTEM',
+   content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+   subTitle: 'Algunas Funciones',
+   functions: ['coki1','coki2','coki3'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/logo-blue.png'
+ },{
+   title: 'COMUNICATION',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'ACCOUTING',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'TOUR OPERATION',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'POINT OF SALES BILLING',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'PAYROLL',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'MULTIPROPERTY',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'FIXED ASSET',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'MANAGMENT',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'ONLINE RESERVATION',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ },{
+   title: 'MAIL SERVICE',
+   content: 'Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetro.',
+   subTitle: 'Algunas Funciones',
+   functions: ['Operaciones sobre Habitaciones','Operaciones sobre Follos','Reservas'],
+   button: 'SOLICITA MAS INFORMACIÓN',
+   url: 'img/img1.png'
+ }
+];
+
+$li = $('li');
+$a = $('.a');
+$o = $('.oval');
+
+$a.hover( function(e){
+  e.preventDefault();
+  $li.removeClass('current');
+  $(this).parent().addClass('current');
+  var id = ( parseInt(this.dataset.id));
+  $('.content-title').text(data[id].title);
+  $('.content-body').text(data[id].content);
+  $('.content-sub-title').text(data[id].subTitle);
+  var li = data[id].functions.map( e => `<li class="function">${e}</li>`);
+  $('#img').addClass('animated fadeInRight').attr("src", data[id].url);
+  setTimeout(function(){ $('#img').removeClass("animated fadeInRight"); }, 300);//Actualiza la animación
+  $("#function").html(li);
+});
+
+
+/*SECTION 2 *************************/
+var data2 = [
+  {
+    logo: "img/logo-blue.png",
+    text1: "Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de una forma muy eficiente controlar las operaciones diarias con reportes muy útiles y facilidad de manipulación de grandes cantidades de información. Front Desk contiene el modulo de Front Parámetros independiente al operativo, ya que está enfocado a los departamentos de mercadeo y recepción (Jefe de Recepción).",
+    title: "COMPRAS",
+    text2: "Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de u",
+    img: "img/compras.png",
+    fondo: "img/f1.png"
+  },
+  {
+    logo: "img/compras.png",
+    text1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title: "COMPRAS",
+    text2: "Front Desk, con sus más de 160 opciones entre operativas, configuración y reportes, es a nivel de hoteles el más importante entre los módulos. Una vez configurado el Front Parámetros, este módulo nos permite de u",
+    img: "img/compras.png",
+    fondo: "img/logo-blue.png"
+  },
+];
+
+$o.hover( function(e){
+  e.preventDefault();
+  var id = (parseInt(this.dataset.id));
+  $('#logo').addClass('animated bounceIn').attr("src", data2[id].logo);
+  setTimeout(function(){ $('#logo').removeClass("animated bounceIn"); }, 300);//Actualiza la animación
+  $(".text-seven").text(data2[id].text1);
+  $(".compras").text(data2[id].text1);
+  $(".compras-text").text(data2[id].text1);
+  $('#compras').attr("src", data2[id].img);
+  $(".f1").css("background-image", `url( ${data2[id].fondo})`);
+
+});
